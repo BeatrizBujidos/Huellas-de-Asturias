26c66080e0aaf50f17bae30324677d144d40e2dd
Beatriz Bujidos Hernandez <beatrizbujidos@gmail.com>
2026-04-10 11:51:28 +0200

    Actualización WebConfig

--- backend/ProyectoFinal/src/main/java/com/ibq/ProyectoFinal/config/WebConfig.java
+++ backend/ProyectoFinal/src/main/java/com/ibq/ProyectoFinal/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration(proxyBeanMethods = false)
-@EnableWebMvc
 
 class WebConfig implements WebMvcConfigurer {
 
